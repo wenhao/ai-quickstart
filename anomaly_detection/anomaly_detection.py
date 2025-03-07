7bc8c7c13a505c804aa75fbdd0d5c10fd2c6a3ae
wenhao <wenhao@126.com>
2025-03-07 18:36:44 +0800

    [feat]anomaly.

---
+++ anomaly_detection/anomaly_detection.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib as mlp
+from scipy.stats import norm
+import numpy as np
+
+data = pd.read_csv('anomaly_data.csv')
+
+print(data.head())
+
+figure = plt.figure()
+plt.scatter(data.loc[:, 'x1'], data.loc[:, 'x2'])
+plt.title('anomaly data')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.show()
+
+x1 = data.loc[:, 'x1']
+x2 = data.loc[:, 'x2']
+
+font2 = {'family': 'Microsoft YaHei', 'weight': 'normal', 'size': '20'}
+mlp.rcParams['font.family'] = 'Microsoft YaHei'
+mlp.rcParams['axes.unicode_minus'] = False
+figure2 = plt.figure(figsize=(20, 7))
+plt.subplot(121)
+plt.hist(x1, bins=100)
+plt.title('x1 数据分布统计', font2)
+plt.xlabel('x1', font2)
+plt.ylabel('出现次数', font2)
+plt.subplot(122)
+plt.hist(x2, bins=100)
+plt.title('x2 数据分布统计', font2)
+plt.xlabel('x2', font2)
+plt.ylabel('出现次数', font2)
+plt.show()
+
+x1_mean = x1.mean()
+x1_sigma = x1.std()
+x2_mean = x2.mean()
+x2_sigma = x2.std()
+
+print('x1_mean:', x1_mean, 'x1_sigma:', x1_sigma, 'x2_mean:', x2_mean, 'x2_sigma:', x2_sigma)
+
+x1_range = np.linspace(0, 20, 300)
+x1_normal = norm.pdf(x1_range, x1_mean, x1_sigma)
+x2_range = np.linspace(0, 20, 300)
+x2_normal = norm.pdf(x2_range, x2_mean, x2_sigma)
+figure3 = plt.figure(figsize=(20, 7))
+plt.subplot(121)
+plt.plot(x1_range, x1_normal)
+plt.title('x1 正态分布', font2)
+plt.subplot(122)
+plt.plot(x2_range, x2_normal)
+plt.title('x2 正态分布', font2)
+plt.show()
