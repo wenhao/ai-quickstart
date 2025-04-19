b5a103d973a8a1eb0faab0161474851e3434659d
wenhao <wenhao@126.com>
2025-04-19 09:21:04 +0800

    update

--- rag/modescope.py
+++
@@ -1,5 +0,0 @@
-from modelscope import snapshot_download
-
-model_qweb_dir = snapshot_download(model_id="Qwen/Qwen2.5-0.5B-Instruct", cache_dir="/Users/wenhao/dev/modelscope")
-
-#model_bge_dir = snapshot_download(model_id="BAAI/bge-base-zh-v1.5", cache_dir="/Users/wenhao/dev/rag")
